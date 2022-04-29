b0e6febb00b3ecf58d10bb7b2c0e0c00cbeb770c
Rhysling <rkummer@polson.com>
2022-04-29 09:10:11 -0700

    Color card null checks. Live.

--- BotanicaStoreBack.ColorCards/CardBuilder.cs
+++ BotanicaStoreBack.ColorCards/CardBuilder.cs
@@ -81,13 +81,16 @@ namespace BotanicaStoreBack.ColorCards
 			dc.Sections[0].Blocks.Add(pTitle);
 
 			// Family
-			var pFamily = new Paragraph(dc);
-			pFamily.ParagraphFormat = pfNormal.Clone();
-			var rFamily = new Run(dc);
-			rFamily.Text = plant.Family;
-			rFamily.CharacterFormat = new CharacterFormat() { FontName = "Times New Roman", Size = 15.0, Bold = true };
-			pFamily.Inlines.Add(rFamily);
-			dc.Sections[0].Blocks.Add(pFamily);
+			if (!String.IsNullOrWhiteSpace(plant.Family))
+			{
+				var pFamily = new Paragraph(dc);
+				pFamily.ParagraphFormat = pfNormal.Clone();
+				var rFamily = new Run(dc);
+				rFamily.Text = plant.Family;
+				rFamily.CharacterFormat = new CharacterFormat() { FontName = "Times New Roman", Size = 15.0, Bold = true };
+				pFamily.Inlines.Add(rFamily);
+				dc.Sections[0].Blocks.Add(pFamily);
+			}
 
 			// Glyph
 			var pGlyph = new Paragraph(dc);
@@ -99,19 +102,22 @@ namespace BotanicaStoreBack.ColorCards
 			dc.Sections[0].Blocks.Add(pGlyph);
 
 			// Description
-			var pDesc = new Paragraph(dc);
-			pDesc.ParagraphFormat = pfNormal.Clone();
-			var rDesc = new Run(dc);
-			rDesc.Text = plant.Description;
-			rDesc.CharacterFormat = new CharacterFormat() { FontName = "Times New Roman", Size = 15.0, Bold = true };
-			pDesc.Inlines.Add(rDesc);
-			dc.Sections[0].Blocks.Add(pDesc);
+			if (!String.IsNullOrWhiteSpace(plant.Description))
+			{
+				var pDesc = new Paragraph(dc);
+				pDesc.ParagraphFormat = pfNormal.Clone();
+				var rDesc = new Run(dc);
+				rDesc.Text = plant.Description;
+				rDesc.CharacterFormat = new CharacterFormat() { FontName = "Times New Roman", Size = 15.0, Bold = true };
+				pDesc.Inlines.Add(rDesc);
+				dc.Sections[0].Blocks.Add(pDesc);
+			}			
 
 			// Size
 			var pSize = new Paragraph(dc);
 			pSize.ParagraphFormat = pfTopMargin.Clone();
 			var rSize = new Run(dc);
-			rSize.Text = plant.PlantSize;
+			rSize.Text = plant.PlantSize ?? "";
 			rSize.CharacterFormat = new CharacterFormat() { FontName = "Times New Roman", Size = 15.0, Bold = true };
 			pSize.Inlines.Add(rSize);
 			dc.Sections[0].Blocks.Add(pSize);
@@ -120,7 +126,7 @@ namespace BotanicaStoreBack.ColorCards
 			var pType = new Paragraph(dc);
 			pType.ParagraphFormat = pfNormal.Clone();
 			var rType = new Run(dc);
-			rType.Text = plant.PlantType;
+			rType.Text = plant.PlantType ?? "";
 			rType.CharacterFormat = new CharacterFormat() { FontName = "Times New Roman", Size = 15.0, Bold = true };
 			pType.Inlines.Add(rType);
 			dc.Sections[0].Blocks.Add(pType);
@@ -129,7 +135,7 @@ namespace BotanicaStoreBack.ColorCards
 			var pZone = new Paragraph(dc);
 			pZone.ParagraphFormat = pfNormal.Clone();
 			var rZone = new Run(dc);
-			rZone.Text = plant.PlantZone;
+			rZone.Text = plant.PlantZone ?? "";
 			rZone.CharacterFormat = new CharacterFormat() { FontName = "Times New Roman", Size = 15.0, Bold = true };
 			pZone.Inlines.Add(rZone);
 			
