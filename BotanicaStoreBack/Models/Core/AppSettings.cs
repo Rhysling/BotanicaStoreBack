6479d7f2be11cb6a1d5626ff04506ca843d51cfa
Rhysling <rkummer@polson.com>
2026-04-13 08:21:44 -0700

    Cleanup complete.

--- BotanicaStoreBack/Models/Core/AppSettings.cs
+++ BotanicaStoreBack/Models/Core/AppSettings.cs
@@ -1,31 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿namespace BotanicaStoreBack.Models.Core;
 
-namespace BotanicaStoreBack.Models.Core
+public class AppSettings
 {
-	public class AppSettings
-	{
-		public string BotanicaStoreDb_ConnectionString { get; set; }
-		public string BotanicaStoreAdminPw { get; set; }
-		public double TaxRate { get; set; }
-		public AS_Jwt Jwt { get; set; }
-		public AS_Mailgun Mailgun { get; set; }
-		public bool IsDev { get; set; }
-	}
+	public string BotanicaStoreDb_ConnectionString { get; set; }
+	public string BotanicaStoreAdminPw { get; set; }
+	public double TaxRate { get; set; }
+	public AS_Jwt Jwt { get; set; }
+	public AS_Mailgun Mailgun { get; set; }
+	public bool IsDev { get; set; }
+}
 
-	public class AS_Jwt
-	{
-		public string Key { get; set; }
-		public string Issuer { get; set; }
-	}
+public class AS_Jwt
+{
+	public string Key { get; set; }
+	public string Issuer { get; set; }
+}
 
-	public class AS_Mailgun
-	{
-		public string AuthValue { get; set; }
-		public string BaseUrl { get; set; }
-		public string Domain { get; set; }
-		public string FromEmail { get; set; }
-	}
+public class AS_Mailgun
+{
+	public string AuthValue { get; set; }
+	public string BaseUrl { get; set; }
+	public string Domain { get; set; }
+	public string FromEmail { get; set; }
 }
